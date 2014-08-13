75e8101e082d8ca6cec7c2cf75045aeca1863a8b
navy <leiwei1990@gmail.com>
2014-08-13 22:11:10 +0800

    Done: Jump Game; Done: Count and Say

--- count_and_say.cpp
+++ count_and_say.cpp
@@ -2,9 +2,53 @@
        File: count_and_say.cpp
          By: navy
       Email: leiwei1990@gmail.com
-Create Time: 2014-55-12 22:08:46
+Create Time: 2014-08-12 22:08:46
+
+Count and Say 
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+Note: The sequence of integers will be represented as a string.
 ************************************************************************/
 
+#include <string>
+#include <sstream>
 #include <iostream>
 using namespace std;
 
+
+class Solution
+{
+public:
+	string countAndSay(int n)
+	{
+		stringstream ss;
+		string curStr("1");
+		
+		while(--n)
+		{
+			for(int i = 0; i < curStr.size();)
+			{
+				int j = i, cnt = 0;
+				while(j < curStr.size() && curStr[j] == curStr[i]) j++, cnt++;
+				ss<<cnt<<curStr[i];
+				i = j;
+			}
+			ss>>curStr;
+			ss.clear();
+		}
+
+		return curStr;
+	}
+};
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	cout<<(new Solution())->countAndSay(n);
+}
--- jump_game.cpp
+++ jump_game.cpp
@@ -2,9 +2,47 @@
        File: jump_game.cpp
          By: navy
       Email: leiwei1990@gmail.com
-Create Time: 2014-55-12 22:08:36
+Create Time: 2014-08-12 22:08:36
+
+Jump Game
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+For example:
+A = [2,3,1,1,4], return true.
+A = [3,2,1,0,4], return false.
 ************************************************************************/
 
 #include <iostream>
 using namespace std;
 
+class Solution
+{
+public:
+	bool canJump(int A[], int n)
+	{
+		if(1 == n) return A[0] >= 0;				// if only 1 element
+
+		for(int i = 1; i < n && i <= A[i-1]; i++)	// mark each element the farest position it can go
+		{
+			if(i+A[i] >= n-1)						// reach last element
+				return true;
+
+			if(i+A[i] < A[i-1])						
+				A[i] = A[i-1];
+			else
+				A[i] = i + A[i];
+		}
+
+		return false;
+	}
+};
+
+int main()
+{
+	//int A[] = {2,3,1,1,4};
+	//int A[] = {3,2,1,0,4};
+	//int A[] = {1,1,1,1,1};
+	int A[] = {3,0,0,0};
+	cout<<(new Solution())->canJump(A, sizeof(A)/sizeof(0));
+}
