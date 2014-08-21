822c5c5e6e345b6630a298f402dee1ef714c9a94
navy <leiwei1990@gmail.com>
2014-08-21 21:24:36 +0800

    Done: Combination Sum

--- combination_sum.cpp
+++ combination_sum.cpp
@@ -28,12 +28,8 @@ public:
 	{
 		vector<vector<int>> result;
 		sort(c.begin(), c.end());
-		for(int i = 0; i < c.size(); i++)
-		{
-			if(target < c[i])	return result;
-			vector<int> solution;
-			combinationSum(c, i, target-c[i], solution, result);
-		}
+		vector<int> solution;
+		combinationSum(c, 0, target, solution, result);
 
 		return result;
 	}
@@ -41,22 +37,22 @@ public:
 	void combinationSum(vector<int> &c, int start, int target, 
 			vector<int> &solution, vector<vector<int>> &result)
 	{
-		if(start >= c.size())	return ;
+		if(start >= c.size() || target < c[start])	return ;
 		solution.push_back(c[start]);
 		if(c[start] == target)
 		{
 			result.push_back(solution);
 		} else {
-			combinationSum(c, start, target-c[start], solution, result);
-			combinationSum(c, start+1, target-c[start], solution, result);
+			combinationSum(c, start, target-c[start], solution, result);	// includes c[start]
 		}
 		solution.pop_back();
+		combinationSum(c, start+1, target, solution, result);	// not includes c[start]
 	}
 };
 
 int main()
 {
-	int A[] = {2,3,6,7};
+	int A[] = {2,3,5};
 	vector<int> c(&A[0], &A[0]+sizeof(A)/sizeof(0));
 
 	vector<vector<int>> result = (new Solution())->combinationSum(c, 7);
