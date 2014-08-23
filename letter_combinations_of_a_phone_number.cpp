6e11b07fed4cf192cac7ad5c5116645f8576214e
navy <leiwei1990@gmail.com>
2014-08-23 15:25:24 +0800

    Done: Letter Combinations; Done: Palindrome Partitioning

--- letter_combinations_of_a_phone_number.cpp
+++ letter_combinations_of_a_phone_number.cpp
@@ -3,8 +3,52 @@
          By: navy
       Email: leiwei1990@gmail.com
 Create Time: 2014-08-13 23:57:14
+
+Letter Combinations of a Phone Number
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+0:			1:			2: abc			3: def			4: ghi
+5: jkl		6: mno		7: pqrs			8: tuv			9: wxyz
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 ************************************************************************/
 
-#include <iostream>
-using namespace std;
+#include "tools.h"
 
+string maps[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+class Solution
+{
+public:
+	vector<string> letterCombinations(string digits)
+	{
+		vector<string> result;
+
+		letterCombinations(digits, 0, "", result);
+		return result;
+	}
+
+	void letterCombinations(string &digits, int i, 
+			string str, vector<string> &result)
+	{
+		if(i == digits.size())
+		{
+			result.push_back(str);
+			return ;
+		}
+		for(int j = 0; j < maps[digits[i]-'0'].size(); j++)		// depth-first search
+		{
+			letterCombinations(digits, i+1, str+maps[digits[i]-'0'][j], result);
+		}
+	}
+};
+
+int main()
+{
+	string s;
+	cin>>s;
+	vector<string> result = (new Solution())->letterCombinations(s);
+	print(result);
+}
--- palindrome_partitioning.cpp
+++ palindrome_partitioning.cpp
@@ -3,8 +3,71 @@
          By: navy
       Email: leiwei1990@gmail.com
 Create Time: 2014-08-13 23:57:49
+
+Palindrome Partitioning
+Given a string s, partition s such that every substring of the partition is a palindrome.
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+[
+    ["aa","b"],
+	["a","a","b"]
+]
 ************************************************************************/
 
-#include <iostream>
-using namespace std;
+#include "tools.h"
+
+class Solution
+{
+public:
+	vector<vector<string>> partition(string s)
+	{
+		vector<vector<string>> result;
+		vector<string> solution;
+
+		partition(s, 0, solution, result);
+		return result;
+	}
+
+	void partition(string &s, int start, vector<string> &solution, vector<vector<string>> &result)
+	{
+		if(start == s.size())	// found one
+		{
+			result.push_back(solution);
+			return ;
+		}
+		for(int i = start+1; i <= s.size(); i++)	// 依次考虑长度为1,2,3,..的子串是否是回文串
+		{
+			if(isPalindrome(s, start, i))			// 如果是，就递归的求后面部分的所有回文分割
+			{
+				solution.push_back(s.substr(start, i-start));
+				partition(s, i, solution, result);
+				solution.pop_back();
+			}
+		}
+	}
+
+	bool isPalindrome(string &s, int start, int end)
+	{
+		while(start < end)
+		{
+			if(s[start] != s[end-1])
+				return false;
+
+			start++, end--;
+		}
+
+		return true;
+	}
+};
+
+int main()
+{
+	string s;
+	cin>>s;
+	vector<vector<string>> result = (new Solution())->partition(s);
+	print(result);
+	return 0;
+}
 
